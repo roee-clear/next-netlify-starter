21e5091be622557be21a13f2e7fae60bad8f3bb1
roee-clear <roee.chaimowicz@clearme.com>
2022-08-01 17:14:03 +0300

    install netlify

--- pages/admin.js
+++ pages/admin.js
@@ -1,5 +1,23 @@
-export default function Admin() {
-    return (
-      <>ass</>
-    )
-  }
+import dynamic from 'next/dynamic';
+
+const config = {
+ 
+};
+
+const CMS = dynamic(
+  () =>
+    import('netlify-cms-app').then(async (cms) => {
+    //  const widget = await import('netlify-cms-widget-mdx').then((module) => module);
+
+      cms.init({ config });
+
+    //   cms.registerWidget('mdx', widget.MdxControl, widget.MdxPreview)
+    }),
+  { ssr: false, loading: () => <p>Loading Admin...</p> },
+);
+
+const Admin = () => {
+  return <CMS />;
+};
+
+export default Admin;
