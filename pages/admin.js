bfd87a1ce89fad9c7cff3d887982063f57182c61
roee-clear <roee.chaimowicz@clearme.com>
2022-08-02 14:11:31 +0300

    config_15

--- pages/admin.js
+++ pages/admin.js
@@ -1,7 +1,39 @@
 import dynamic from 'next/dynamic';
 
-const config = {
- 
+const config = {  
+    cms_manual_init: true,
+    backend: {
+      name: 'github',
+      repo: 'roee-clear/next-netlify-starter',
+      branch: 'main',
+    },
+    media_folder: 'public/img',
+    public_folder: 'img',
+    collections: [
+      {
+        name: 'pages',
+        label: 'Pages',
+        files: [
+          {
+            label: 'Home',
+            name: 'home',
+            file: 'content/pages/home.md',
+            fields: [
+              {
+                label: 'Hero Title',
+                name: 'hero_title',
+                widget: 'string',
+              },
+              {
+                label: 'Hero Description',
+                name: 'hero_description',
+                widget: 'markdown',
+              },
+            ],
+          },
+        ],
+      },
+    ],  
 };
 
 const CMS = dynamic(
@@ -9,7 +41,7 @@ const CMS = dynamic(
     import('netlify-cms-app').then(async (cms) => {
     //  const widget = await import('netlify-cms-widget-mdx').then((module) => module);
 
-      cms.init();
+      cms.init({config});
 
     //   c ms.registerWidget('mdx', widget.MdxControl, widget.MdxPreview)
     }),
